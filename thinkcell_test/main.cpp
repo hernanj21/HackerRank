c508ab892904bc4428f6bb862d242004e998e47c
hernanj21 <jhernan.jimenezg@gmail.com>
2022-10-13 01:52:18 +0100

    Works for all cases now

--- thinkcell_test/main.cpp
+++ thinkcell_test/main.cpp
@@ -20,19 +20,8 @@ public:
 	// and assign must do nothing.
 	void assign(K const& keyBegin, K const& keyEnd, V const& val) {
 		// INSERT YOUR SOLUTION HERE
-		// Here I should edit m_map
-		// REMEMBER TO DELETE IOSTREAM ABOVE AND TRY IT WITH THAT
-		// auto ub = m_map.upper_bound(keyEnd);
-		auto ub = m_map.upper_bound(keyBegin);
-		auto lb = m_map.lower_bound(keyEnd); // This will give the value for the upper limit
-		//std::cout << "Upper bound = begin " << (m_map.upper_bound(keyEnd) == m_map.begin()) << std::endl;
-		//std::cout << "Upper bound = end " << (m_map.upper_bound(keyEnd) == m_map.end()) << std::endl;
-		
-		// auto it = m_map.upper_bound(keyEnd);
-		//std::cout << "aFTER INSERTING Upper bound = begin " << (m_map.upper_bound(keyEnd) == m_map.begin()) << std::endl;
-		//std::cout << "aFTER INSERTING Upper bound = end " << (m_map.upper_bound(keyEnd) == m_map.end()) << std::endl;
-		
-		
+		auto ub = m_map.upper_bound(keyBegin); // Closest up to the lower bound
+		auto lb = m_map.lower_bound(keyEnd); // Closest belos the upper bound
 		if (lb == m_map.begin()) {
 			// If its empty we will assign the end value to the upper limit
 			// If its because there is no lower one so far we do the same
@@ -40,31 +29,19 @@ public:
 			m_map.insert({ keyEnd, m_valBegin });
 		}
 		else {
-			// If not, we set the corresponding value which is the one of lowerbound
-			
-			// For the case of when the next interval is just after an existing one
-			// and the lower bound corresponds to the upper bound of the previous one
-			
 			V valEnd = (--lb)->second;
-			
-			if (ub == lb) {
-				m_map.erase(lb);
+			auto check = m_map.find(keyBegin);
+			if (check != m_map.end()) { // When the first key coincides with another first key we have problems
+				// We just replace the new value
+				m_map[keyBegin] = val;
+				m_map.insert({ keyEnd, valEnd });
 			}
-
-			m_map.erase(ub, ++lb); // ++ because i decreased it before
-
-			m_map.insert({ keyEnd, valEnd }); // INsert value of lower bound (previous one bc lower bound is itself)
-			m_map.insert({ keyBegin, val }); // Insert the asked value
-			std::cout << "Inside the else one time" << std::endl;
+			else {
+				m_map.erase(ub, ++lb);
+				m_map.insert({ keyEnd, valEnd }); // INsert value of lower bound (previous one bc lower bound is itself)
+				m_map.insert({ keyBegin, val }); // Insert the asked value
+			}
 		}
-
-
-		/*for (auto it = ++m_map.find(keyBegin); it != m_map.find(keyEnd); it++) {
-			m_map.erase(it);
-		}*/
-
-		// Now we need to delete what it's in the middle
-
 	}
 
 	// look-up of the value associated with key
@@ -117,8 +94,8 @@ int main() {
 	M.assign(7, 9, 2);*/
 
 	// Add one just next to the other
-	M.assign(2, 6, 1);
-	M.assign(6, 9, 2);
+	/*M.assign(2, 6, 1);
+	M.assign(6, 9, 2);*/
 
 	// Add in a border intersection
 	/*M.assign(2, 6, 1);
@@ -126,9 +103,9 @@ int main() {
 	M.assign(4, 8, 3);*/
 
 	// Add in a sepparated intersection
-	/*M.assign(7, 9, 1);
+	M.assign(7, 9, 1);
 	M.assign(2, 5, 2);
-	M.assign(4, 8, 3);*/
+	M.assign(4, 8, 3);
 
 	while (q != 11) {
 		/*std::cout << "Insert query: ";
